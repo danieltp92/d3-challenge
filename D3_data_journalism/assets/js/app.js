c5a61f47346f6e1cbda60b34517bf45e461bed2d
danieltp92 <daniel.tp92@gmail.com>
2021-06-11 13:57:56 -0500

    design parameters for plot

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,18 @@
-// @TODO: YOUR CODE HERE!
+
+var svgHeight = 700;
+var svgWidth = 1200;
+var margin = {
+    top: 10,
+    bottom: 50,
+    right: 20,
+    left: 40
+}
+
+var height = svgHeight - margin.bottom - margin.top;
+var width = svgWidth - margin.left - margin.right;
+
+var scat = d3.select(#scatter).append('div').classed('chart', true);
+
+var svg = scat.append('svg').attr('height', svgHeight).attr('width', svgWidth);
+
+
